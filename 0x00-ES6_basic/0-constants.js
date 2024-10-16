a6bcbc5368a665802c1b47c6993e1493f4c297ff
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-10-16 03:50:32 -0700

    jhg

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,19 @@
+// Function using const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
+// Helper function to return a string
 export function getLast() {
   return ' is okay';
 }
 
+// Function using let
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast();
+  combination += getLast(); // Reassigning 'combination'
 
   return combination;
 }
+
