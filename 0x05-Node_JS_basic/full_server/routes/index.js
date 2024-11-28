b524bada3d7e62ce2dcbdd067ce7027e864e99c7
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-11-28 10:48:18 +0000

    number 8 commit

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,13 +1,17 @@
-// full_server/routes/index.js
-import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const router = express.Router();
+/**
+ * Binding the routes to the appropriate handler in the
+ * given Express application.
+ * @param {Express} app The Express application.
+ * @author Kelvin Mutea <https://github.com/Zulu-Inventor33>
+ */
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
 
-router.get('/', AppController.getHomepage);
-router.get('/students', StudentsController.getAllStudents);
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
-
-export default router;
-
+export default mapRoutes;
+module.exports = mapRoutes;
