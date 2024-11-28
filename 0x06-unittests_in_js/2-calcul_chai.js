a841838e1c58e828d462aa862158bff9910c178a
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-11-28 11:40:52 +0000

    number two commit

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+  // Round the numbers first
+  a = Math.round(a);
+  b = Math.round(b);
+
+  // Perform operations based on type
+  if (type === 'SUM') {
+    return a + b;
+  } else if (type === 'SUBTRACT') {
+    return a - b;
+  } else if (type === 'DIVIDE') {
+    if (b === 0) {
+      return 'Error';
+    }
+    return a / b;
+  }
+}
+
+module.exports = calculateNumber;
